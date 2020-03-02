bbe54f1c351e6a39173837e6c0f7e716fe966a76
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2020-03-03 01:07:06 +0900

    feat: add configure for firebase.🔥

---
+++ webpack.config.firebase.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, '/index.ts'),
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {loader: 'ts-loader'},
+          {loader: 'eslint-loader'}
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts'],
+    modules: [path.join(__dirname, 'src'), 'node_modules']
+  },
+  output: {
+    libraryTarget: 'this',
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  externals: [nodeExternals()]
+};
