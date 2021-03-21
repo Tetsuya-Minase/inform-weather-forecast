b7dca9ebc338e4a2100c328a094900f773541252
Tetsuya Minase <tetsuya.minasen@gmail.com>
2021-03-22 00:38:47 +0900

    add url to message🐥 (#20)
    * feat: add url to message🐥
    
    * fix: test code
    
    * fix: test code2

--- __tests__/domain/service/converter-service.test.ts
+++ __tests__/domain/service/converter-service.test.ts
@@ -74,6 +74,7 @@ describe('test converter service today', () => {
       '重ね着：layeringComment1',
       '熱中症：heatstrokeComment1',
       'ビール：beerComment1',
+      'url: https://weather.yahoo.co.jp/weather/13/4410.html',
     ].join('\n');
     expect(service.toDetailInformation(index, weatherDate, temperature, DATE.TODAY)).toEqual(correctData);
   });
@@ -134,6 +135,7 @@ describe('test converter service tomorrow', () => {
       '重ね着：layeringComment2',
       '熱中症：heatstrokeComment2',
       'ビール：beerComment2',
+      'url: https://weather.yahoo.co.jp/weather/13/4410.html',
     ].join('\n');
     expect(service.toDetailInformation(index, weatherDate, temperature, DATE.TOMORROW)).toEqual(correctData);
   });
