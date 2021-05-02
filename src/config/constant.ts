4da3a8bf01554f84c029d60a79aba8302e0ef016
Tetsuya Nishi <tetsuya.minasen@gmail.com>
2021-05-02 13:02:21 +0900

    feat: ignore configs

--- src/config/constant.ts
+++
@@ -1,2 +0,0 @@
-export const WEATHER_FORECAST_AT_TOKYO = 'https://weather.yahoo.co.jp/weather/13/4410.html';
-export const WEATHER_FORECAST_AT_SAITAMA = 'https://weather.yahoo.co.jp/weather/jp/11/4310.html';
